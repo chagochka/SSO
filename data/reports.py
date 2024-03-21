8e3f2f2a6029e7e125da99c25e43ce59cdac8a0c
chagochka <vad.ermin@yandex.ru>
2024-03-21 19:20:21 +0700

    начал писать настройки для админа

--- data/reports.py
+++ data/reports.py
@@ -4,7 +4,6 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
 
